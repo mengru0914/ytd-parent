543ddc4cdd95c1b8b6ad87db58e77633c82d50ce
xian <3337664135@qq.com>
2019-08-05 16:08:28 +0800

    郭仕杰第二次提交 跑通了eureka user

--- ytd_user/src/main/java/com/ytd/controller/LoginController.java
+++ ytd_user/src/main/java/com/ytd/controller/LoginController.java
@@ -7,11 +7,13 @@ import com.ytd.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
+@RequestMapping("/goLogin")
 public class LoginController {
 
     @Autowired
