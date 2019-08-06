833e7005e37e5a8fb507c8cd7799c2548aa97c83
xian <3337664135@qq.com>
2019-08-06 15:57:28 +0800

    xian第四次提交

--- ytd_openaccount/src/main/java/com/ytd/controller/OpenController.java
+++ ytd_openaccount/src/main/java/com/ytd/controller/OpenController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OpenController {
 
     @RequestMapping("/toopen")
-    public void toopen(){
-
+    public String toopen(){
+        return "openaccount";
     }
 }
